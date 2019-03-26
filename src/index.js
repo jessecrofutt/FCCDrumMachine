073844b21de7fb450bfdb637cf7a4f4b7eebcd7a
jessecrofutt <jessecrofutt@gmail.com>
2019-03-26 22:57:12 +0100

    removed unused audio files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// import _ from 'lodash';
-// import './scss/FCCDrummer.scss';
-// import swirl from './media/spiral-starburst4.png';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { hot } from 'react-hot-loader/root'
@@ -15,22 +11,3 @@ ReactDOM.render(
     document.getElementById('drum-machine')
 );
 
-// ReactDOM.render(
-//     <div>{title}</div>,
-//     document.getElementById('drum-machine')
-// );
-
-// module.hot.accept();
-
-// function component() {
-//   let element = document.createElement('div');
-//
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('drum-machine');
-//   let myIcon = new Image();
-//   myIcon.src= swirl;
-//   element.appendChild(myIcon);
-//   return element;
-// }
-//
-// document.body.appendChild(component());
