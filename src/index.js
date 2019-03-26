b0a8958fedf2259254d7a8211869ad2d50e07541
jessecrofutt <jessecrofutt@gmail.com>
2019-03-26 23:04:42 +0100

    updated bundle.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { hot } from 'react-hot-loader/root'
 import DrumMachine from './js/drumMachine.jsx';
-
-
-
 
 ReactDOM.render(
     <DrumMachine/>,
