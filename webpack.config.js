14e279f97f0cc26265abe759597623d890205396
jessecrofutt <jessecrofutt@gmail.com>
2019-04-03 23:15:04 +0200

    Rebuild Bundle 16

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/dist',
-    publicPath:  '/dist',
+    publicPath:  '/dist/',
     filename: 'bundle.js'
   },
   resolve: {
