3ef7d30b89ddadf00725ae162beecd1204fe21e1
jessecrofutt <jessecrofutt@gmail.com>
2019-04-03 22:27:03 +0200

    Rebuild Bundle 11

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/dist',
-    publicPath:  '/dist',
+    publicPath:  '/mist',
     filename: 'bundle.js'
   },
   resolve: {
